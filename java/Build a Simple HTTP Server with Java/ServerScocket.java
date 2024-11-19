c6d355a6dbc81567a882ddc0acb19b03e7b455b6
qrh <1508945593@qq.com>
2024-11-19 11:20:32 +0800

    add ServerSocket

---
+++ java/Build a Simple HTTP Server with Java/ServerScocket.java
@@ -0,0 +1,10 @@
+import java.net.ServerSocket;
+public class SimpleHTTPServer{
+    public static void main(String[] args) throws Exception{
+        final ServerSocket server = new ServerSocket(8081))
+        System.out.println("Listening for connection on port 8081....");
+        while(true){
+            
+        }
+    })
+}
